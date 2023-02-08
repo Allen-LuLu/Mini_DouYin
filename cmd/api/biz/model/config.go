f13050dc36b136b13be50bb5bc6050114003ea0a
Allen-LuLu <962729318@qq.com>
2023-02-08 16:22:13 +0800

    配置文件相关结构定义

--- cmd/api/biz/model/config.go
+++ cmd/api/biz/model/config.go
@@ -5,6 +5,7 @@ type Cfg struct{
 	UserCfg userCfg	 `ini:"UserService"` // UserService相关配置
 	EtcdCfg etcdCfg  `ini:"etcd"` // etcd相关配置
 	ApiCfg apiCfg `ini:"api"` // api相关配置
+	RedisCfg redisCfg `ini:"redis"`
 }
 
 
@@ -21,4 +22,10 @@ type etcdCfg struct{
 type apiCfg struct {
 	Addr string `ini:"addr"`
 	ServiceName string `ini:"serviceName"`
+}
+
+type redisCfg struct {
+	Host string `ini:"host"`
+	PassWord string `ini:"password"`
+	DBNum int `ini:"dbNum"`
 }
