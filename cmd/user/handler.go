56c566b78301b1c3c5f37a872acb102126e987e6
Allen-LuLu <962729318@qq.com>
2023-02-08 16:19:33 +0800

    重新实现服务逻辑，将token鉴权移除，放到api层统一处理

--- cmd/user/handler.go
+++ cmd/user/handler.go
@@ -3,14 +3,11 @@ package main
 import (
 	"Mini_DouYin/cmd/user/dao"
 	"Mini_DouYin/common/consts/errmsg"
-	token2 "Mini_DouYin/common/pkg/token"
 	"Mini_DouYin/kitex_gen/user"
 	"context"
 	"errors"
 	"github.com/cloudwego/hertz/pkg/protocol/consts"
-	"github.com/go-redis/redis"
 	"gorm.io/gorm"
-	"time"
 )
 
 // UserServiceImpl implements the last service interface defined in the IDL.
@@ -22,116 +19,70 @@ func (s *UserServiceImpl) Register(ctx context.Context, req *user.RegisterReq) (
 	resp = new(user.RegisterResp)
 	resp.Base = new(user.BaseResp)
 
-	_,err = dao.GetUserIdByUserName(req.Username)  // 查询用户名是否已存在数据库
-	if !errors.Is(err,gorm.ErrRecordNotFound){  // 如果err不是找不到，说明数据库中可能已经存在相同的用户名
-		if err == nil { // err为nil，说明数据库中可能已经存在相同的用户名
+	_, err = dao.GetUserIdByUserName(req.Username) // 到数据库查询用户是否存在
+	if !errors.Is(err, gorm.ErrRecordNotFound) { // 如果err不是ErrRecordNotFound，说明数据库中可能存在着该用户的记录，或者出现其他错误
+		if err == nil { // 说明数据库中可能存在着该用户的记录，返回通知客户端修改用户名
 			resp.Base.Code = consts.StatusBadRequest
 			resp.Base.Errmsg = errmsg.USERALREADYEXITS
-			return resp,nil
+			return resp, nil
 		}
-		return nil,err
+		return nil, err
 	}
 
-	user,err := dao.CreatUser(req.Username,req.Password)  // 创建用户
+	user, err := dao.CreatUser(req.Username, req.Password) // 数据库中创建新的用户记录
 
 	if err != nil {
-		return nil,err
+		return nil, err
 	}
 
-	token,err := token2.GenToken(time.Now(),user.UserID,nil) // 生成token
-	if err != nil {
-		resp.Base.Code = consts.StatusInternalServerError
-		resp.Base.Errmsg = errmsg.GENTOKENFAILED
-		return resp,nil
-	}
 
-	err = dao.StoreToken(user.UserID,token,token2.Expire*time.Second) // 保存token
-	if err != nil {
-		resp.Base.Code = consts.StatusInternalServerError
-		resp.Base.Errmsg = errmsg.STORETOKENFAILED
-		return resp,nil
-	}
-
+	resp.UserID = user.UserID
 	resp.Base.Code = consts.StatusOK
 	resp.Base.Errmsg = "ok"
-	resp.UserID = user.UserID
-	resp.Token = token
-	return resp,nil
+	return resp, nil
 }
 
-// Login implements the UserServiceImpl interface.
-func (s *UserServiceImpl) Login(ctx context.Context, req *user.LoginReq) (resp *user.LoginResp, err error) {
+// CheckUser implements the UserServiceImpl interface.
+// 该服务只负责加密密码并到数据库中匹配记录，匹配成功则返回用户id
+func (s *UserServiceImpl) CheckUser(ctx context.Context, req *user.CheckUserReq) (resp *user.CheckUserResp, err error) {
 	// TODO: Your code here...
-	resp = new(user.LoginResp)
-	resp.Base = new(user.BaseResp)
+	resp = new(user.CheckUserResp)
 
-	userID,err := dao.CheckUser(req.Username,req.Password) //检查用户名密码是否匹配
-	if err != nil || userID < 0{
-		resp.Base.Code = consts.StatusBadRequest
-		resp.Base.Errmsg = errmsg.CHECKUSERFAILED
-		return resp,nil
+	userID, err := dao.CheckUser(req.Username, req.Password) // 到数据库中匹配记录
+	if err != nil || userID < 0 {
+		return resp, err
 	}
 
-	token,err := dao.QueryToken(userID) // 查询token是否过期
-	if err != nil {
-		if !errors.Is(err,redis.Nil){ // 如果err是redis.Nil，说明redis中找不到该用户的token了，需要重新生成，其它错误则直接返回
-			resp.Base.Code = consts.StatusInternalServerError
-			resp.Base.Errmsg = errmsg.QUERYTOKEN
-			return resp,nil
-		}
-	}
-
-	if token == ""{
-		token,_ := token2.GenToken(time.Now(),userID,nil) // 重新生成token
-		err = dao.StoreToken(userID,token,token2.Expire*time.Second) // 存储token
-		if err != nil {
-			resp.Base.Code = consts.StatusInternalServerError
-			resp.Base.Errmsg = errmsg.STORETOKENFAILED
-			return resp,nil
-		}
-	}
-
-	resp.Base.Code = consts.StatusOK
-	resp.Base.Errmsg = "ok"
 	resp.UserID = userID
-	resp.Token = token
-
-	return resp,nil
+	return resp, nil
 }
 
 // UserInfo implements the UserServiceImpl interface.
+// 该服务负责查询给定的id列表相对应的用户信息
+// 在api层jwt鉴权通过后才能调用该服务
 func (s *UserServiceImpl) UserInfo(ctx context.Context, req *user.UserInfoReq) (resp *user.UserInfoResp, err error) {
 	// TODO: Your code here...
 	resp = new(user.UserInfoResp)
 	resp.Base = new(user.BaseResp)
-	resp.User = new(user.User)
+	resp.Users = make([]*user.User,0)
 
-	token,err := dao.QueryToken(req.UserID)  // 根据用户id查询token
-	if err != nil {
-		resp.Base.Code = consts.StatusBadRequest
-		resp.Base.Errmsg = errmsg.AUTHFAILED
-		return resp,nil
-	}
-
-	if token != req.Token{ // 校验客户端传来的token
-		resp.Base.Code = consts.StatusBadRequest
-		resp.Base.Errmsg = errmsg.AUTHFAILED
-		return resp,nil
-	}
-
-	claim,_ := token2.ParseToken(token) // 解析token
-
-	user,err := dao.GetUserByID(claim.UserId) // 获取用户信息
+	users, err := dao.GetUserByID(req.UserIDS)
 	if err != nil {
 		resp.Base.Code = consts.StatusBadRequest
 		resp.Base.Errmsg = errmsg.USERNOTEXIST
-		return resp,nil
+		return resp, nil
 	}
 	resp.Base.Code = consts.StatusOK
 	resp.Base.Errmsg = "ok"
-	resp.User.Id = user.UserID
-	resp.User.Name = user.UserName
-	resp.User.FollowCount = user.FollowCount
-	resp.User.FollowerCount = user.FollowerCount
-	return resp,nil
+	for _,u := range users{
+		resp.Users = append(resp.Users,&user.User{
+			Id:            u.UserID,
+			Name:          u.UserName,
+			FollowCount:   u.FollowCount,
+			FollowerCount: u.FollowerCount,
+		})
+	}
+	return
 }
+
+
